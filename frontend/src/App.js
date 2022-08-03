035130d2c860bb48ecbe721121ee7b3d7a5d38c3
Marius Isoardi <99771633+marius13127@users.noreply.github.com>
2022-08-03 02:00:12 +0200

    Send token to localStorage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
-
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import SignUpForm from "./pages/SignUpForm";
 import SignInForm from "./pages/SignInForm";
-
+import {UidContext} from "./components/AppContext";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/signin" element={<SignInForm/>}></Route>
-                <Route path="/signup" element={<SignUpForm/>}></Route>
-                <Route path="/" element={<Home/>}></Route>
+
+    const [uid,setUid] = useState(null)
+    useEffect(async () => {
+        await axios({
+        })
+    })
+
+    return (() =>
+        <div>
+            <UidContext.Provider value={uid}>
+
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/signin" element={<SignInForm/>}></Route>
+                        <Route path="/signup" element={<SignUpForm/>}></Route>
+                        <Route path="/" element={<Home/>}></Route>
 
 
+                    </Routes>
+                </BrowserRouter>
+            </UidContext.Provider>
 
-            </Routes>
-        </BrowserRouter>
+        </div>
     );
 };
 
